701c36a4636e1124744b9b4638bd7c918e3848ab
Andrey <54776640+qPexLegendary@users.noreply.github.com>
2020-09-24 12:37:04 +0700

    First commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class Bot:
+
+    def __init__(self, token: str, group_id: int):
+        self.server = self.ts = self.key = ""
+        self.token = token
+        self.group_id = group_id
+
+    def update_longpoll(self):
+        response = self.send_request("groups.getLongPollServer", {
+            'group_id': self.group_id
+        })['response']
+
+        self.server = response['server']
+        self.key = response['key']
+        self.ts = response['ts']
+
+    # Старт
+    def start(self):
+        self.update_longpoll()
+
+        while True:
+            print(self.get_longpoll_data())
+
+    # Отправка запороса к api серверу
+    def send_request(self, method: str, params: dict) -> dict:
+        params['access_token'] = self.token
+        params['v'] = "5.124"
+
+        return requests.post("https://api.vk.com/method/" + method, params).json()
+
+    # Получение данных из longpoll сервера
+    def get_longpoll_data(self) -> dict:
+        while True:
+            request = requests.post(f"{self.server}?act=a_check&key={self.key}&ts={self.ts}&wait=25").json()
+
+            if "failed" not in request:
+                break
+
+            if request['failed'] == 1:
+                self.ts = int(request['ts'])
+                continue
+
+            self.update_longpoll()
+        self.ts = request['ts']
+        return request
---
+++ main.py
@@ -0,0 +1,6 @@
+import bot
+
+
+if __name__ == '__main__':
+    bot_object = bot.Bot(token="", group_id=0)
+    bot_object.start()
