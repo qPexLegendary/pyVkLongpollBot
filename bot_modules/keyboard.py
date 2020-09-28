86d7ee06ab4e449243a0ed805b3448e583dea87b
Andrey <54776640+qPexLegendary@users.noreply.github.com>
2020-09-28 19:29:50 +0700

    Добавлены новые типы кнопок для клавиатуры

--- bot_modules/keyboard.py
+++ bot_modules/keyboard.py
@@ -15,24 +15,63 @@ class Keyboard:
         self.buttons = []
         self.selected_line = 0
 
+    # Добавить кнопку в список кнопок
+    def add_button_to_list(self, button: dict):
+        if len(self.buttons) == self.selected_line:
+            self.buttons.append([button])
+        else:
+            self.buttons[self.selected_line].append(button)
+
     # Добавить обычную кнопку
-    def add_simple_button(self, label: str, color: str = "secondary", payload=None):
+    def add_text_button(self, label: str, color: str = "secondary", payload=None):
         if payload is None:
             payload = {}
 
-        button = {
+        self.add_button_to_list({
             'action': {
                 'type': "text",
                 'label': label,
                 'payload': encoder.JSONEncoder().encode(payload),
             },
             'color': color,
-        }
+        })
 
-        if len(self.buttons) == self.selected_line:
-            self.buttons.append([button])
-        else:
-            self.buttons[self.selected_line].append(button)
+    # Добавить кнопку с ссылкой
+    def add_open_link_button(self, label: str, url: str = "https://vk.com"):
+        self.add_button_to_list({
+            'action': {
+                'type': "open_link",
+                'link': url,
+                'label': label,
+                'payload': "{}",
+            }
+        })
+
+    # Добавить кнопку с VK Pay
+    def add_vk_pay_button(self, payload=None, pay_hash: str = ""):
+        if payload is None:
+            payload = {}
+
+        self.add_button_to_list({
+            'action': {
+                'type': "vkpay",
+                'payload': encoder.JSONEncoder().encode(payload),
+                'hash': pay_hash,
+            }
+        })
+
+    # Добавить CallBack кнопку
+    def add_callback_button(self, title: str = "", payload=None):
+        if payload is None:
+            payload = {}
+
+        self.add_button_to_list({
+            'action': {
+                'type': "callback",
+                'title': title,
+                'payload': encoder.JSONEncoder().encode(payload),
+            }
+        })
 
     # Перейти на следущую стоку клавиатуры
     def next_line(self):
