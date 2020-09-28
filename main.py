d3ecbaf27c7bbcec0134cb2a13ad08d4dbd2e70a
Andrey <54776640+qPexLegendary@users.noreply.github.com>
2020-09-28 17:08:35 +0700

    Изменена стилистика кода в main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import bot_exceptions as ex
 
 if __name__ == '__main__':
     try:
-        bot.Bot(token="", group_id=0).start()
+        bot.Bot(
+            token="",
+            group_id=0
+        ).start()
     except ex.AuthFail:
         print("Введены неверные данные для подключения!")
